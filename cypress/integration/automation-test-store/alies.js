cce9da740ad6fc9482dcf1c8a1d56537a69f8452
Damith Anuradha <damith.anuradha@ifsworld.com>
2021-01-16 17:53:03 +0530

    Test with alies

--- cypress/integration/automation-test-store/alies.js
+++ cypress/integration/automation-test-store/alies.js
@@ -6,16 +6,31 @@ describe("Home page item test",()=>{
     it("Chacking the cart icon available",()=>{
 
         cy.visit('https://automationteststore.com/');
-        cy.get('div .thumbnail').each(($ele,index,list)=>{
-            cy.wrap($ele).find('.productcart').should('have.attr','title','Add to Cart');
+        cy.get('div .thumbnail').find('.productcart')   .each(($ele,index,list)=>{
+            cy.wrap($ele).should('have.attr','title','Add to Cart');
             // cy.wrap($ele).find('.productcart').invoke('attr','title').should('include','Add to Cart');
         })
 
-    })
+    });
 
     it('number of item in home page checking',()=>{
         cy.visit('https://automationteststore.com/');
         cy.get('div .thumbnail').should('have.lengthOf',16);
 
+    });
+
+    it.only("Item price checing", ()=>{
+        cy.visit('https://automationteststore.com/');
+        var array=[];
+        var sum=0;
+        cy.get('div .thumbnail').find('.oneprice').each(($element,index,list)=>{
+            
+          sum+=(parseFloat($element.text().split('$')[1]));
+            
+          cy.log(sum);
+        })
+
     })
+
+
 })
